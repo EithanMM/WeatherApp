0e4c4cb8d4775d864cf5877318972b65f8270f75
EithanMM <eithanhoulahan@gmail.com>
2019-06-28 23:41:19 -0600

    adding Celsius tag to legend

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,7 @@ const forecast = (latitude, longitude, callback) => {
 				body.daily.data[0].summary +
 					' It is currently ' +
 					body.currently.temperature +
-					' degrees out. There is a ' +
+					' degrees Celsius out. There is a ' +
 					body.currently.precipProbability +
 					'% chance of rain.'
 			);
